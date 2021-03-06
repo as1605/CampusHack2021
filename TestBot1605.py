e52a8ae300d1ff031b1f021788b6cc2c58316fc9
Aditya Singh <adityasingh@Adityas-MacBook-Air.local>
2021-03-06 21:10:45 +0530

    modularised code with functions

--- TestBot1605.py
+++ TestBot1605.py
@@ -4,14 +4,73 @@ import json
 from bs4 import BeautifulSoup
 import urllib.parse
 
-PATH="../chromedriver.exe"
-import chromedriver_binary
-import os
-import sys
-
-
-
+TOKEN="ODE3MzgwODcxNDU1Mzc1NDAw.YEIrQQ.dXNWOi9Dvv3ePnebssNsFl1Igow"
 client = discord.Client()
+
+def stack(q, n=3):
+    link="https://api.stackexchange.com/2.2/search/advanced"
+    args= {
+        "order" : "desc",
+        "sort" : "votes",
+        "q" : q[:1000],
+        "site" : "stackoverflow"
+    }
+    response=requests.get(url=link, params=args)
+    response=response.json()
+    a=[]
+    for i in range(n):
+        try:
+            a.append(response["items"][i]["link"])
+        except:
+            continue
+    return a
+
+def duck(str, n=5):
+    r = requests.get('https://duckduckgo.com/html/?q='+str, headers={'user-agent': 'my-app/0.0.1'}) 
+    soup = BeautifulSoup(r.text, 'html.parser')
+    results = soup.find_all('a', attrs={'class':'result__a'}, href=True)
+    a=[]
+    for link in results:
+        n=n-1
+        url = link['href']
+        o = urllib.parse.urlparse(url)
+        d = urllib.parse.parse_qs(o.query)
+        a.append(d['uddg'][0])
+        if n<=0:
+            break
+    return a
+
+def rex(code, inp, plat='g++'):
+    link='https://rextester.com/rundotnet/api'
+    args= {
+        "LanguageChoice": 7,
+        "Program": code,
+        "Input": inp,
+        "CompilerArgs": "source_file.cpp -o a.out"
+    }
+    response = requests.post(link, data=args).json()
+    a={}
+    try:
+        a["Result"]=response.get("Result")
+    except:
+        a["Result"]=None
+    try:
+        a["Warnings"]=response.get("Warnings")
+    except:
+        a["Warnings"]=None
+    try:
+        a["Errors"]=response.get("Errors")
+    except:
+        a["Errors"]=None
+    try:
+        a["Stats"]=response.get("Stats")
+    except:
+        a["Stats"]=None
+    try:
+        a["Files"]=response.get("Files")
+    except:
+        a["Files"]=None
+    return a
 
 @client.event
 async def on_ready():
@@ -19,69 +78,47 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("```Errors:"):
-        err = message.content.split("Errors:")[1]
-        err = err.split('```')[0]
-        link="https://api.stackexchange.com/2.2/search/advanced"
-        args= {
-            "order" : "desc",
-            "sort" : "votes",
-            "q" : err,
-            "site" : "stackoverflow"
-        }
-        stack=requests.get(url=link, params=args)
-        ans=stack.json()
-        await message.channel.send(ans["items"][0]["link"])
-        await message.channel.send(ans["items"][1]["link"])
-        await message.channel.send(ans["items"][2]["link"])
+
     if message.author == client.user:
         return
-    if (message.author.nick==None):
-        name=message.author.name
-    else:
-        name=message.author.nick
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello! '+ name)
+
     if message.content.lower().find('nice')>=0:
         await message.channel.send('nice')
-    if message.content.startswith('g++'):
-        url='https://rextester.com/rundotnet/api'
-        req= {
-            "LanguageChoice": 7,
-            "Program": message.content.split("```c++")[1],
-            "Input": message.content.split("```txt")[1],
-            "CompilerArgs": "source_file.cpp -o a.out"
-        }
-        rep = requests.post(url, data=req).json()
-        await message.channel.send("```"+str(rep.get("Result"))+"```")
-        await message.channel.send("```Warnings: "+str(rep.get("Warnings"))+"```")
-        await message.channel.send("```Errors: "+str(rep.get("Errors"))+"```")
-        await message.channel.send("```Stats: "+str(rep.get("Stats"))+"```")
-        await message.channel.send("```Files: "+str(rep.get("Files"))+"```")
-        if(str(rep.get("Errors"))!="None"):
-            errors=str(rep.get("Errors")).split(':')
-            index=errors.index(' error')
-            parsing=errors[index+1]
-            
-            await message.channel.send ("This is the precise error:"+parsing)
-            s=parsing+' stack overflow'
-            r = requests.get('https://duckduckgo.com/html/?q='+s, headers={'user-agent': 'my-app/0.0.1'}) 
-            soup = BeautifulSoup(r.text, 'html.parser')
-            results = soup.find_all('a', attrs={'class':'result__a'}, href=True)
+    
+    if message.content.startswith("stack"):
+        str = message.content.split("stack")[1]
+        ques=stack(str)
+        for q in ques:
+            await message.channel.send(q[:2000])
 
-            for link in results:
-                url = link['href']
-                o = urllib.parse.urlparse(url)
-                d = urllib.parse.parse_qs(o.query)
-                await message.channel.send(d['uddg'][0])
-                    if (type(rep.get("Result"))==str):
-            await message.channel.send("```"+rep.get("Result")[:1000]+"```")
-        if (type(rep.get("Warnings"))==str):
-            await message.channel.send("```Warnings: "+rep.get("Warnings")[:1000]+"```")
-        if (type(rep.get("Stats"))==str):      
-            await message.channel.send("```Stats: "+rep.get("Stats")[:1000]+"```")
-        if (type(rep.get("Errors"))==str):      
-            await message.channel.send("```Errors: "+rep.get("Errors")[:1000]+"```")
-        if (type(rep.get("Files"))==str):      
-            await message.channel.send("```Files: "+rep.get("Files")[:1000]+"```")
-client.run("ODE3MzgwODcxNDU1Mzc1NDAw.YEIrQQ.gxw6UTKW7hXMWBckARswyg-FKpE")
+    if message.content.startswith("duck"):
+        str = message.content.split("duck")[1]
+        ques=duck(str)
+        for q in ques:
+            await message.channel.send(q[:2000])
+
+    if message.content.startswith("g++"):
+        judge= rex(message.content.split("```c++")[1],message.content.split("```txt")[1],"g++")
+        if judge["Result"]!=None:
+            await message.channel.send("```"+judge["Result"][:2000]+"```")
+        if judge["Warnings"]!=None:
+            await message.channel.send("```Warnings: "+judge["Warnings"][:2000]+"```")
+        if judge["Stats"]!=None:
+            await message.channel.send("```Stats: "+judge["Stats"][:2000]+"```")
+        if judge["Errors"]!=None:
+            await message.channel.send("```Errors: "+judge["Errors"][:2000]+"```")
+        if judge["Files"]!=None:
+            await message.channel.send("```Files: "+judge["Files"][:2000]+"```")
+
+        if judge["Errors"]!=None:
+            err=judge["Errors"].split(':')
+            index=err.index(' error')
+            err=err[index+1]
+            ques=stack(err)
+            for q in ques:
+                await message.channel.send(q[:2000])
+            links=duck(err)
+            for link in links:
+                await message.channel.send(link[:2000])
+
+client.run(TOKEN)
