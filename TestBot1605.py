4f7bf877c24e2eb70a39cb2129fd9f13b77289e1
Aditya Singh <adityasingh@Adityas-MacBook-Air.local>
2021-03-06 16:56:23 +0530

    gives stackoverflow links for "```Errors: ...```"

--- TestBot1605.py
+++ TestBot1605.py
@@ -10,6 +10,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.content.startswith("```Errors:"):
+        err = message.content.split("Errors:")[1]
+        err = err.split('```')[0]
+        link="https://api.stackexchange.com/2.2/search/advanced"
+        args= {
+            "order" : "desc",
+            "sort" : "votes",
+            "q" : err,
+            "site" : "stackoverflow"
+        }
+        stack=requests.get(url=link, params=args)
+        ans=stack.json()
+        await message.channel.send(ans["items"][0]["link"])
+        await message.channel.send(ans["items"][1]["link"])
+        await message.channel.send(ans["items"][2]["link"])
     if message.author == client.user:
         return
     if (message.author.nick==None):
@@ -30,14 +45,13 @@ async def on_message(message):
         }
         rep = requests.post(url, data=req).json()
         if (type(rep.get("Result"))==str):
-            await message.channel.send("```"+rep.get("Result")+"```")
+            await message.channel.send("```"+rep.get("Result")[:1000]+"```")
         if (type(rep.get("Warnings"))==str):
-            await message.channel.send("```Warnings: "+rep.get("Warnings")+"```")
+            await message.channel.send("```Warnings: "+rep.get("Warnings")[:1000]+"```")
         if (type(rep.get("Stats"))==str):      
-            await message.channel.send("```Stats: "+rep.get("Stats")+"```")
+            await message.channel.send("```Stats: "+rep.get("Stats")[:1000]+"```")
         if (type(rep.get("Errors"))==str):      
-            await message.channel.send("```Errors: "+rep.get("Errors")+"```")
+            await message.channel.send("```Errors: "+rep.get("Errors")[:1000]+"```")
         if (type(rep.get("Files"))==str):      
-            await message.channel.send("```Files: "+rep.get("Files")+"```")
-
+            await message.channel.send("```Files: "+rep.get("Files")[:1000]+"```")
 client.run("ODE3MzgwODcxNDU1Mzc1NDAw.YEIrQQ.gxw6UTKW7hXMWBckARswyg-FKpE")
